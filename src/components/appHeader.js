a8b5cdd714ac49051e7a5f10e2f231c6ce30c248
[MEADOW_DEV] <bendixon50@gmail.com>
2017-06-27 22:48:12 +0100

    :sparkles: Starting win32 window controls

--- src/components/appHeader.js
+++ src/components/appHeader.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { Bounce, Entrance } from 'animate-components';
+import { Entrance, SlideRight } from 'animate-components';
 
 export default class AppHeader extends React.Component {
     render() {
+        var doRenderWindowControls = null;
+        if (process.platform === 'win32') {
+            doRenderWindowControls = (
+                <SlideRight duration="1s" as="div" className="appWin32WindowControls">
+                    <img id="windowControlMinimize" className="iconWindowControls" src="../static/svg/windowControl_Minimize.svg"/>
+                    <img id="windowControlMaximize" className="iconWindowControls" src="../static/svg/windowControl_Maximize.svg"/>
+                    <img id="windowControlClose" className="iconWindowControls" src="../static/svg/windowControl_Close.svg"/>
+                </SlideRight>
+            );
+        }
+
         return (
             <div className="app appHeader">
                 <Entrance as="h1" duration="2.5s">
                     Overwatch <span>Stat</span>
                 </Entrance>
+                {doRenderWindowControls}
             </div>
         )
     }
