8e939666e824e9fbd38adaa15b1259a21fccd132
[MEADOW_DEV] <bendixon50@gmail.com>
2017-06-28 14:23:20 +0100

    :hammer: Changed window controls animation to fade

--- src/components/appHeader.js
+++ src/components/appHeader.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Entrance, SlideRight } from 'animate-components';
+import { Entrance, FadeIn } from 'animate-components';
 
 export default class AppHeader extends React.Component {
     render() {
         var doRenderWindowControls = null;
         if (process.platform === 'win32') {
             doRenderWindowControls = (
-                <SlideRight duration="1s" as="div" className="appWin32WindowControls">
+                <FadeIn duration="1s" as="div" className="appWin32WindowControls">
                     <img id="windowControlMinimize" className="iconWindowControls" src="../static/svg/windowControl_Minimize.svg"/>
                     <img id="windowControlMaximize" className="iconWindowControls" src="../static/svg/windowControl_Maximize.svg"/>
                     <img id="windowControlClose" className="iconWindowControls" src="../static/svg/windowControl_Close.svg"/>
-                </SlideRight>
+                </FadeIn>
             );
         }
 
