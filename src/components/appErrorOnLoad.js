1f189c0bf3b53243110a2ff9dde94d9681b9d723
[MEADOW_DEV] <bendixon50@gmail.com>
2017-07-06 22:19:21 +0100

    :hammer: Added unknown error to appErrorOnLoad component

--- src/components/appErrorOnLoad.js
+++ src/components/appErrorOnLoad.js
@@ -18,6 +18,10 @@ const errorText = {
     no_internet: {
       text_enUS: 'No Internet Connection',
       text_kr: '인터넷에 연결되어 있지 않습니다'
+    },
+    unknown: {
+      text_enUS: 'Unkown error',
+      text_kr: '알수없는 오류'
     }
   }
 }
@@ -29,7 +33,7 @@ export default class AppErrorOnLoad extends React.Component {
         <div className='appErrorContent'>
           <Zoom duration='2.5s' as='img' id='errorOnLoadIcon' draggable='false' src='../static/svg/status_offline.svg' />
           <Zoom duration='1.5s'>
-            <h1>{errorText.header.prefix.text_enUS} <span>{errorText.header.postfix.no_internet.text_enUS}</span></h1>
+            <h1>{errorText.header.prefix.text_enUS} <span>{errorText.header.postfix.unknown.text_enUS}</span></h1>
             <p>{errorText.sub_header.no_internet.text_enUS}</p>
           </Zoom>
         </div>
