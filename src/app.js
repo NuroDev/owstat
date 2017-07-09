3d701297d33269a240470c73e6997f4318efac8b
[MEADOW_DEV] <bendixon50@gmail.com>
2017-07-09 22:58:47 +0100

    :art: Moved to ES6 import

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import * as appLog from 'electron-log'
 
 import AppHeader from './components/appHeader'
 import AppRegionSelector from './components/appRegionSelect'
 import AppStatus from './components/appStatus'
 
-const appLog = require('electron-log')
-
 export default class App extends React.Component {
+  componentDidMount () {
+    appLog.info('| RENDER | App loaded |')
+  }
+
   render () {
-    appLog.info('| RENDER | Online: Loading app |')
     return (
       <div>
         <AppHeader />
